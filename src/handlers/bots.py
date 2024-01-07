c91d81ecddcd354a2a3dc62d3d59044154bcf5d4
Eugene B <evgeniy.b@softvoya.com>
2024-01-07 19:27:46 +0300

    update url

--- src/handlers/bots.py
+++ src/handlers/bots.py
@@ -259,7 +259,7 @@ async def bot_deploy(bot: AsyncTeleBot, call: types.CallbackQuery, db, data, buc
     await bot.send_message(call.message.chat.id, _('Start building your bot. Please, wait...'))
     print('...building...')
     result = await docker.images.build(
-        remote='https://ebondarikk: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@github.com/chillingturtle/tg-bot.git',
+        remote='https://ebondarikk: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@github.com/chillingturtle/tg-bot.git',
         nocache=True,
         tag='bot_image',
         buildargs={
