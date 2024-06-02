07a6baa093e979aecd620494b1076e213a13deb2
Eugene B <evgeniy.b@softvoya.com>
2024-06-02 15:05:30 +0300

    build

---
+++ build.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+cd ~/tg-bot
+
+# Получите аргументы командной строки
+TG_TOKEN=$1
+TG_OWNER_CHAT_ID=$2
+TG_BOT_NAME=$3
+TG_BOT_ID=$4
+TG_BOT_OWNER_USERNAME=$5
+FIREBASE_TOKEN=$6
+FIREBASE_PROJECT=$7
+
+# Установите конфигурацию firebase
+firebase --project=${FIREBASE_PROJECT} --token ${FIREBASE_TOKEN} functions:config:set telegram.token="${TG_TOKEN}"
+firebase --project=${FIREBASE_PROJECT} --token ${FIREBASE_TOKEN} functions:config:set telegram.owner_chat_id="${TG_OWNER_CHAT_ID}"
+firebase --project=${FIREBASE_PROJECT} --token ${FIREBASE_TOKEN} functions:config:set telegram.name="${TG_BOT_NAME}"
+firebase --project=${FIREBASE_PROJECT} --token ${FIREBASE_TOKEN} functions:config:set telegram.bot_owner_username="${TG_BOT_OWNER_USERNAME}"
+firebase --project=${FIREBASE_PROJECT} --token ${FIREBASE_TOKEN} functions:config:set telegram.bot_id="${TG_BOT_ID}"
+
+# Получите конфигурацию firebase
+firebase functions:config:get > .runtimeconfig.json
+
+# Разверните функции firebase
+firebase --project=${FIREBASE_PROJECT} --token ${FIREBASE_TOKEN} deploy --only functions:${TG_BOT_NAME}
